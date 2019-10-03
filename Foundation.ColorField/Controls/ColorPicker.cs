cc845f78f14d32f34a0bb8fafc11c27e97099818
EAST\yamada <yamada@est.co.jp>
2019-10-03 14:55:27 +0900

    Add project files.

---
+++ Foundation.ColorField/Controls/ColorPicker.cs
@@ -0,0 +1,23 @@
+﻿using Sitecore.Globalization;
+using Sitecore.Shell.Applications.ContentEditor;
+using System.Web.UI;
+
+namespace Foundation.ColorField.Controls
+{
+    public class ColorPicker : Text
+    {
+        public ColorPicker()
+        {
+            Class = "scContentControlColorPicker scContentControl";
+            Width = new System.Web.UI.WebControls.Unit(100);
+        }
+
+        protected override void DoRender(HtmlTextWriter output)
+        {
+            Attributes["placeholder"] = Translate.Text(Placeholder);
+            SetWidthAndHeightStyle();
+            output.Write("<input" + ControlAttributes + " type=\"color\">");
+            RenderChildren(output);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,12 @@
+# Foundation.ColorField
+A simple implementation of color field for Sitecore XP.
+
+## Installation
+1. Download the package from [Github Release](https://github.com/xirtardauq/Foundation.ColorField/releases) page.
+2. Install the package to your Sitecore.
+
+## License
+This software is released under the MIT License, see license.txt.  
+
+## Author
+- T.Yamada (xirtardauq@gmail.com)
